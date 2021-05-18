d6aa67c4e0826cc562c378515ea261e3e61507e4
ShaninIvan <shaninivan1993@gmail.com>
2021-05-18 11:35:23 +0300

    public

--- src/components/other/footerTotal.js
+++
@@ -1,5 +0,0 @@
-import { Component } from "../../core/Component";
-
-const footerTotal = new Component('footerTotal');
-
-footerTotal.createBody(`Active tasks: %ready%, Finished tasks: %finished%`);
